58fe68259c3fa96e8c783d49ac6d90bb2dc9ae5d
Peter Jang <peter.x.jang@gmail.com>
2020-12-18 10:13:23 -0600

    Allow web requests from local HTML files

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'example.com'
+    origins "localhost:8080", "example.com", "*"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
