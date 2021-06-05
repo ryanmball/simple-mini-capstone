ab8416af084f83de1374cfb0f1e84117840a7338
ryan <ball.ryan.rb@gmail.com>
2021-06-05 15:49:42 -0600

    adds productDestroy function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/products" => "api/products#index"
   post "/products" => "api/products#create"
   get "/products/:id" => "api/products#show"
-  patch "/products/:id" => "api/products#update"
+  post "/products/:id" => "api/products#update"
   delete "/products/:id" => "api/products#destroy"
 end
--- products.js
+++ products.js
@@ -4,25 +4,26 @@ var app = new Vue({
   el: "#app",
   data: function() {
     return {
-      message: "Hello from JavaScript!",
       products: [],
       name: "",
       description: "",
       price: "",
       image_url: "",
+      id: "",
+      message: false,
     };
   },
   created: function() {  // responsible for executing code as soon as the DOM is loaded
-    this.productsIndex();
+    this.productIndex();
   }, 
   methods: {
-    productsIndex: function() {
+    productIndex: function() {
       axios.get("http://localhost:3000/products").then(response => {
         console.log(response.data);
         this.products = response.data;
       });
     },
-    productsCreate: function() {
+    productCreate: function() {
       var params = {
         name: this.name,
         description: this.description,
@@ -31,7 +32,28 @@ var app = new Vue({
       };
       axios.post("http://localhost:3000/products", params).then(response => {
         console.log(response.data);
-        this.products.unshift(response.data);
+        this.products.push(response.data);
+      });
+      this.name = "";
+      this.description = "";
+      this.price = "";
+      this.image_url = "";
+    },
+    // productUpdate: id => {
+    //   var params = {
+    //     name: this.name,
+    //     description: this.description,
+    //     price: parseFloat(this.price),
+    //     image_url: this.image_url,
+    //   };
+    //   axios.post(`http://localhost:3000/products/${id}`, params).then(response => {
+    //     console.log(response.data);
+    //     this.message = true;
+    //   });
+    // },
+    productDestroy: id => {
+      axios.delete(`http://localhost:3000/products/${id}`).then(response => {
+        console.log(response.data);
       });
     },
   }
